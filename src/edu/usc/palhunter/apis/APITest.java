2a0f2abe651b922a711bb32ad79db58d74b2274b
unknown <tranvanluan7@gmail.com>
2014-11-26 17:34:30 -0800

    updateLocation api

---
+++ src/edu/usc/palhunter/apis/APITest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.usc.palhunter.apis;
+
+import edu.usc.infolab.roadnetwork.GeoPoint;
+import edu.usc.infolab.roadnetwork.IGeoPoint;
+import edu.usc.palhunter.business.TrajectoryManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Luan
+ */
+public class APITest {
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        TrajectoryManager trajectoryManager = new TrajectoryManager();
+        GeoPoint point = new GeoPoint(12.4,12.54) ;
+        trajectoryManager.updateLocation(5, point);
+    }
+    
+}
--- src/edu/usc/palhunter/business/TrajectoryManager.java
+++ src/edu/usc/palhunter/business/TrajectoryManager.java
@@ -2,18 +2,51 @@ package edu.usc.palhunter.business;
 
 import java.sql.SQLException;
 
-
 import edu.usc.infolab.roadnetwork.IGeoPoint;
 import edu.usc.palhunter.db.DBHelper;
 
 public class TrajectoryManager {
-  private DBHelper db = new DBHelper();
-  private static final String TABLE_NAME = "TRAJECTORY";
 
-  public void updateLocation(int userId, IGeoPoint point)
-      throws ClassNotFoundException, SQLException {
-    String sql = String.format("insert into %s(lat, lng) values(?, ?)",
-        TABLE_NAME, point.getLat(), point.getLng());
-    db.executeQuery(sql);
-  }
+    private DBHelper db = new DBHelper();
+    private static final String TABLE_NAME = "TRAJECTORY";
+    private static final String CURRENT_LOCATION_TABLE = "CURRENT_LOCATION";
+
+    public void updateLocation(int userId, IGeoPoint point)
+            throws ClassNotFoundException, SQLException {
+//        String sql ="Insert into "+TABLE_NAME+"(time, lat, lng, user_id, location) values(CURRENT_TIMESTAMP,\n"
+//                +point.getLat()+ ","+ point.getLng()+", "+userId+", SDO_GEOMETRY(2001, 8307, \n"
+//                + "    SDO_POINT_TYPE("+point.getLat()+","+point.getLng()+",NULL), NULL, NULL))"
+//                ;
+//        db.executeQuery(sql);
+        
+        String sql = "declare exist number; begin "+
+                "select count(user_id) into exist  from TRAJECTORY where user_id = "+userId+ ";"+
+                " if exist > 0 then "+
+                "update TRAJECTORY set time =CURRENT_TIMESTAMP, lat= "+point.getLat()+","+
+                "lng = "+point.getLng()+", location=SDO_GEOMETRY(2001, 8307, \n"
+                + "    SDO_POINT_TYPE("+point.getLat()+","+point.getLng()+",NULL), NULL, NULL) where user_id = "+userId+";\n"
+                + "else \n"+
+                  "insert into TRAJECTORY (time, lat, lng, user_id, location) values (CURRENT_TIMESTAMP, "
+                + point.getLat()+","+point.getLng()+","+userId+","+"SDO_GEOMETRY(2001, 8307, \n"
+                + "    SDO_POINT_TYPE("+point.getLat()+","+point.getLng()+",NULL), NULL, NULL) );"
+                + "    end if;\n"
+                + "end;";
+        
+        db.executeQuery(sql);
+        String updateCurrentLocation = "declare exist number;\n"
+                + "begin\n"
+                + "select count(user_id) into exist from current_location where user_id = "+userId+";\n"
+                + "if exist > 0 then\n"
+                + "update current_location set attime = CURRENT_TIMESTAMP, location = SDO_GEOMETRY(2001, 8307, \n"
+                + "    SDO_POINT_TYPE("+point.getLat()+","+point.getLng()+",NULL), NULL, NULL) where user_id = "+userId+";\n"
+                + "    else \n"
+                + "    insert into current_location (user_id, location, attime) values (\n"
+                + "    "+ userId+ ", SDO_GEOMETRY(2001, 8307, \n"
+                + "    SDO_POINT_TYPE("+point.getLat()+","+point.getLng()+",NULL), NULL, NULL), CURRENT_TIMESTAMP\n"
+                + "    );\n"
+                + "    end if;\n"
+                + "end;";
+        db.executeQuery(updateCurrentLocation);
+
+    }
 }
